a9e0a30e34a42b2ab6a9bd330d8230548c611f16
mylucien <53183558+mylucien@users.noreply.github.com>
2024-01-19 22:34:49 +0800

    Update pro.sh

--- pro/pro.sh
+++ pro/pro.sh
@@ -2,8 +2,19 @@
 
 sudo apt update
 sudo apt install make -y
-git clone -b main --single-branch https://github.com/sdf8057/ipq6000.git openwrt
+git clone -b master --single-branch https://github.com/sdf8057/ipq6000.git openwrt
 cd openwrt
-./scripts/feeds update -a && ./scripts/feeds install -a
+./scripts/feeds update -a
+svn co https://github.com/coolsnowwolf/packages/trunk/net/n2n feeds/packages/net/n2n
+svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-n2n feeds/luci/applications/luci-app-n2n
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/other-plugins/luci-app-adguardhome
+#优先安装 passwall 源
+./scripts/feeds install -a -f -p passwall_packages
+./scripts/feeds install -a -f -p passwall_luci
+./scripts/feeds install -a
+# ttyd免登陆
+sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config
+# design修改proxy链接
+sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
 wget https://raw.githubusercontent.com/ppayjjk/ipq6000/main/pro/.config
 make menuconfig
